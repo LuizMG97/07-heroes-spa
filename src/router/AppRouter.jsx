fc600c0f533077d866ac328cf0d227b0b02bc3c8
Luis <lmiguel@buhoms.com>
2022-10-25 14:30:30 -0500

    Login creado

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,19 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { LoginPage } from "../auth/pages/LoginPage"
-import { DcPage } from "../heroes/pages/DcPage"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
+import { Route, Routes } from "react-router-dom"
+
+import { HeroesRoutes } from "../heroes"
+import { LoginPage } from "../auth"
 
 export const AppRouter = () => {
   return (
     <>
+
         <Routes>
-            <Route path="marvel" element={<MarvelPage/>} />
-            <Route path="dc" element={<DcPage/>} />
 
             <Route path="login" element={<LoginPage/>} />
-            <Route path="/" element={<Navigate to="/marvel"/>} />
+
+            <Route path="/*" element={<HeroesRoutes/>} />
+        
+
         </Routes>
     </>
   )
